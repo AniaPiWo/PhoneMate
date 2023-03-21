db4bd5245d9defe7d6378e72150d537d96739a01
AniaPiWo <snowflake@wp.pl>
2023-03-21 17:58:43 +0100

    .js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,13 +6,13 @@ import { Register } from 'pages/register';
 import { SignIn } from 'pages/sign-in';
 import { Home } from 'pages/home';
 import { Phonebook } from 'pages/phonebook';
-import { Layout } from './Layout/Layout';
-import { Fail } from 'pages/404';
-import { refreshUser } from 'redux/auth/auth.thunk';
-import { useAuth } from 'hook/useAuth/useAuth';
-import { ProtectedRoute } from './protectedRoute';
-import { RestrictedRoute } from './restrictedRoute';
-import { UserMenu } from './UserMenu/UserMenu';
+import { Layout } from './Layout/Layout.js';
+import { Fail } from 'pages/404.js';
+import { refreshUser } from 'redux/auth/auth.thunk.js';
+import { useAuth } from 'hook/useAuth/useAuth.js';
+import { ProtectedRoute } from './protectedRoute.js';
+import { RestrictedRoute } from './restrictedRoute.js';
+import { UserMenu } from './UserMenu/UserMenu.js';
 
 export const App = () => {
   const dispatch = useDispatch();
