f135fe80cc1307c0afccca9ba9fd320ff1186d01
AniaPiWo <snowflake@wp.pl>
2023-03-23 15:55:15 +0100

    fix

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Center, Button, Stack } from "@chakra-ui/react";
+
+import {useNavigate} from 'react-router-dom';
 
 export const Fail = () => {
+    const navigate = useNavigate();
     return (
-            <div className="wrapper">
-                <h1 className="fail-title">404</h1>
-                <p className="fail-p">We found nothing!</p>
-            </div>
+        <Center h="100vh">
+            <Stack spacing={3} align="center">
+                <img src={process.env.PUBLIC_URL + '/404pb-nobg.png'} alt="Nothing found" />
+                <Button w={300} colorScheme="purple" variant="solid" onClick={() => navigate(-1)}>
+                    Go back
+                </Button>
+            </Stack>
+        </Center>
     )
 }
--- src/pages/my-account.js
+++
@@ -1,2 +0,0 @@
-import React from "react";
-import { UserMenu } from "components/UserMenu/UserMenu";
